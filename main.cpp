1c1b839cba550a396a6af89007c44381b79cbab2
RenLin56 <55425518+RenLin56@users.noreply.github.com>
2019-11-12 16:42:45 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,43 +1,35 @@
-// 
-// Created by Solo_Dance on 9/7/2019. 
- // 
- #include <iostream>
- #include "SCORE.h"
+#include "point.h"
+#include<iostream>
+int main()
+{
+	point array[10] = { point(1,3),point(2,4) };
+	point * array_heap = new point[10];
+	*array_heap = point(1, 3);
+	*(array_heap + 1) = point(2, 4);
+	GetLength(array[0], array[1]);
+	GetLength(array_heap[0], array_heap[1]);
+	double *a = new double;
+	double *b = new double;
+	*a = (array_heap + 1)->GetX();
+	*b = (array_heap + 1)->GetY();
+	array[0].SwapAxis(a, b);
+	cout << "array[0]与array_heap[1]交换后,array[0]\t";
+	array[0].display();
+	/*double *t=new double;
+	double *s = new double;
+	*t=array_heap->GetX();
+	*s = array_heap->GetY();
+	double &m = *t;
+	double &n = *s;
+	array[1].SwapPoint(m, n);
+	cout << "第二次交换后,array[0]\t";
+	array[0].display();
+	cout << "array_heap[0]:\t";
+	(array_heap+1)->display();*/
+	//delete[]array_heap;
+	delete a;
+	delete b;
+	delete[]array_heap;
+	system("pause");
 
-
- int main() {
-		printf("******************************\n");
-	 	printf("       学生成绩管理分析系统        \n");
-	 	printf("         Mingfu Yan           \n");
-	 	printf("******************************\n\n");
-	
-
-		 	// 1. 初始化变量 
-		 	int N = 0;
-	 	SS *pstu = nullptr;
-	
-
-		 	// 2. 从文件中读取学生信息 
-		 	pstu = readDataFromFile(&N);
-	
-
-		 	//3. 统计：统计各学生总成绩，计算其成绩排名。 
-		 	calcuScore(pstu, N);
-	
-
-		 	//4.  查询：输入学号可查询某个学生的成绩信息。 
-		 	searchGradeInfo(pstu, N);
-	
-
-		 	//5. 分析：统计全班学生成绩的均值和标准方差，并对成绩分布简要分析。 
-		 	analysisScore(pstu, N);
-	
-
-		 	//6. 释放内存空间 
-		 	delete pstu;
-	
-		 	std::cout << "\n\n------欢迎使用学生成绩管理分析系统!再见!--------\n\n";
-	getchar();
-	 	return 0;
-	
 }
